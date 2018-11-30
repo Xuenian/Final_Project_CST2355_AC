f76d8b9051a53039bb42f626445b4569a9c884ef
claire-hj <hend0286@algonquinlive.com>
2018-11-29 21:06:27 -0500

    some initial work on nutrition db - testing ability to commit/push

--- app/src/main/java/ca/algonquinstudents/cst2335_group_project/Member1MainActivity.java
+++ app/src/main/java/ca/algonquinstudents/cst2335_group_project/Member1MainActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Member1MainActivity extends Activity {
+    Button favButton, searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,10 @@ public class Member1MainActivity extends Activity {
         Button Btn1 = findViewById(R.id.member1Btn2);
         Button Btn2 = findViewById(R.id.member1Btn3);
         Button Btn3 = findViewById(R.id.member1Btn4);
+
+
+        favButton = findViewById(R.id.m1FavButton);
+        searchButton = findViewById(R.id.m1SearchButton);
 
         Btn1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -40,5 +46,29 @@ public class Member1MainActivity extends Activity {
                 Member1MainActivity.this.finish();
             }
         });
+
+
+        favButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(Member1MainActivity.this, M1FavActivity.class);
+                startActivity(intent);
+                Member1MainActivity.this.finish();
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+
+
+    public void onFavAdded(String name){
+        name+=" added to favourites";
+        Toast toast = Toast.makeText(Member1MainActivity.this, name, Toast.LENGTH_LONG); //this is the ListActivity
+        toast.show(); //display your message box
     }
 }
